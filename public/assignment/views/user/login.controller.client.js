febb1632f5c86616d7337d1a58cd4054ea14353b
Sam Davies <ssam.daviess@gmail.com>
2017-02-11 15:40:12 -0500

    login view and controller

--- public/assignment/views/user/login.controller.client.js
+++ public/assignment/views/user/login.controller.client.js
@@ -3,17 +3,32 @@
     .module('WebAppMaker')
     .controller('LoginController', loginController);
 
-  function loginController() {
+  function loginController($location, UserService) {
     var vm = this;
     vm.login = login;
+    vm.register = register;
+    vm.clearError = clearError;
 
-    function init() {
-
-    }
+    function init() {}
     init();
 
     function login(user) {
-      console.log('hit');
+      if (user) {
+        var usr = UserService.findUserByCredentials(user.username, user.password);
+        if (usr) {
+          $location.url('/user/' + usr._id);
+        } else {
+          vm.error = 'User not found. Please try again.'
+        }
+      }
+    }
+
+    function register() {
+      $location.url('/register');
+    }
+
+    function clearError() {
+      vm.error = null;
     }
   }
 })();
