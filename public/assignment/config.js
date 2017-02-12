8e601326bc6f5e2e5c01935f9f121e07216bdae9
Sam Davies <ssam.daviess@gmail.com>
2017-02-12 01:42:49 -0500

    website list controller

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -26,7 +26,9 @@
         controllerAs: 'vm'
       })
       .when('/user/:uid/website', {
-        templateUrl: 'views/website/website-list.view.client.html'
+        templateUrl: 'views/website/website-list.view.client.html',
+        controller: 'WebsiteListController',
+        controllerAs: 'vm'
       })
       .when('/user/:uid/website/new', {
         templateUrl: 'views/website/website-new.view.client.html'
--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -5,12 +5,12 @@
 
   function websiteService() {
     var websites = [
-      { "_id": "123", "name": "Facebook",    "developerId": "456", "description": "Lorem" },
-      { "_id": "234", "name": "Tweeter",     "developerId": "456", "description": "Lorem" },
-      { "_id": "456", "name": "Gizmodo",     "developerId": "456", "description": "Lorem" },
+      { "_id": "123", "name": "Facebook", "developerId": "456", "description": "Lorem" },
+      { "_id": "234", "name": "Tweeter", "developerId": "456", "description": "Lorem" },
+      { "_id": "456", "name": "Gizmodo", "developerId": "456", "description": "Lorem" },
       { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
-      { "_id": "678", "name": "Checkers",    "developerId": "123", "description": "Lorem" },
-      { "_id": "789", "name": "Chess",       "developerId": "234", "description": "Lorem" }
+      { "_id": "678", "name": "Checkers", "developerId": "123", "description": "Lorem" },
+      { "_id": "789", "name": "Chess", "developerId": "234", "description": "Lorem" }
     ];
     return {
       'createWebsite': createWebsite,
@@ -29,7 +29,7 @@
       var userWebsites = [];
       for (var i = 0; i < websites.length; i++) {
         if (websites[i].developerId === userId) {
-          return userWebsites.push(websites[i]);
+          userWebsites.push(websites[i]);
         }
       }
       return userWebsites;
