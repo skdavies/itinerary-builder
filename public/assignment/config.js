e6422ff4aafad4187356b98e9499919bb0529e89
Sam Davies <ssam.daviess@gmail.com>
2017-02-15 19:13:39 -0500

    refactoring

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -6,67 +6,67 @@
   function configuration($routeProvider) {
     $routeProvider
       .when('/login', {
-        templateUrl: 'views/user/login.view.client.html',
+        templateUrl: 'views/user/templates/login.view.client.html',
         controller: 'LoginController',
         controllerAs: 'vm'
       })
       .when('/', {
-        templateUrl: 'views/user/login.view.client.html',
+        templateUrl: 'views/user/templates/login.view.client.html',
         controller: 'LoginController',
         controllerAs: 'vm'
       })
       .when('/register', {
-        templateUrl: 'views/user/register.view.client.html',
+        templateUrl: 'views/user/templates/register.view.client.html',
         controller: 'RegisterController',
         controllerAs: 'vm'
       })
       .when('/user/:uid', {
-        templateUrl: 'views/user/profile.view.client.html',
+        templateUrl: 'views/user/templates/profile.view.client.html',
         controller: 'ProfileController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website', {
-        templateUrl: 'views/website/website-list.view.client.html',
+        templateUrl: 'views/website/templates/website-list.view.client.html',
         controller: 'WebsiteListController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/new', {
-        templateUrl: 'views/website/website-new.view.client.html',
+        templateUrl: 'views/website/templates/website-new.view.client.html',
         controller: 'WebsiteNewController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/:wid', {
-        templateUrl: 'views/website/website-edit.view.client.html',
+        templateUrl: 'views/website/templates/website-edit.view.client.html',
         controller: 'WebsiteEditController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/:wid/page', {
-        templateUrl: 'views/page/page-list.view.client.html',
+        templateUrl: 'views/page/templates/page-list.view.client.html',
         controller: 'PageListController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/:wid/page/new', {
-        templateUrl: 'views/page/page-new.view.client.html',
+        templateUrl: 'views/page/templates/page-new.view.client.html',
         controller: 'PageNewController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/:wid/page/:pid', {
-        templateUrl: 'views/page/page-edit.view.client.html',
+        templateUrl: 'views/page/templates/page-edit.view.client.html',
         controller: 'PageEditController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/:wid/page/:pid/widget', {
-        templateUrl: 'views/widget/widget-list.view.client.html',
+        templateUrl: 'views/widget/templates/widget-list.view.client.html',
         controller: 'WidgetListController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/:wid/page/:pid/widget/new', {
-        templateUrl: 'views/widget/widget-chooser.view.client.html',
+        templateUrl: 'views/widget/templates/widget-chooser.view.client.html',
         controller: 'WidgetNewController',
         controllerAs: 'vm'
       })
       .when('/user/:uid/website/:wid/page/:pid/widget/:wgid', {
-        templateUrl: 'views/widget/widget-edit.view.client.html',
+        templateUrl: 'views/widget/templates/widget-edit.view.client.html',
         controller: 'WidgetEditController',
         controllerAs: 'vm'
       });
