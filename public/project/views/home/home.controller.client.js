3f36a41c8218774e87a29befb62adf04cb60f6c9
Sam Davies <ssam.daviess@gmail.com>
2017-03-26 23:16:21 -0400

    home page improvements

--- public/project/views/home/home.controller.client.js
+++ public/project/views/home/home.controller.client.js
@@ -3,26 +3,25 @@
     .module('ItineraryPlanner')
     .controller('HomeController', homeController);
 
-  function homeController($location, $routeParams, UserService) {
+  function homeController($location, $routeParams, UserService, $scope) {
     var vm = this;
     vm.login = login;
     vm.toggleLogin = toggleLogin;
     vm.toggleRegister = toggleRegister;
     vm.register = register;
+    vm.saveItinerary = saveItinerary;
+    vm.resetToLastSave = resetToLastSave;
 
     function init() {
       vm.userId = $routeParams['userId'];
       vm.itinId = $routeParams['itinId'];
       if (vm.userId && vm.itinId) {
-
+        // TODO get places from itinerary service call
+        // TODO get user
+      } else {
+        vm.places = [];
+        vm.user = false;
       }
-      vm.places = [
-        { name: 'a' },
-        { name: 'b' },
-        { name: 'c' },
-        { name: 'd' }
-      ];
-      vm.user = false;
       initMap()
     }
 
@@ -37,16 +36,23 @@
       var options = {};
       var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'), options);
       window.google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        console.log(autocomplete.getPlace());
+        var place = autocomplete.getPlace();
+        if (vm.places.length === 0) {
+          var coordinates = { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() }
+          new google.maps.Marker({
+            position: coordinates,
+            map: map
+          });
+          map.setCenter(coordinates)
+        }
+        vm.places.push(place);
+        $scope.$apply();
       });
-      // new google.maps.Marker({
-      //   position: fenway,
-      //   map: map
-      // });
     }
 
     function login(user) {
       UserService.findUserByCredentials(user.username, user.password).then(function (user) {
+        vm.toggleLogin();
         vm.user = user;
       });
     }
@@ -57,9 +63,20 @@
         password: user.password
       };
       UserService.createUser(usr).then(function (user) {
+        vm.toggleRegister();
         vm.user = user;
-        //TODO save itinerary
+        if (vm.places.length > 0) {
+          //TODO save itinerary
+        }
       });
+    }
+    
+    function saveItinerary() {
+
+    }
+    
+    function resetToLastSave() {
+      
     }
 
     function toggleRegister() {
