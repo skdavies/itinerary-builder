9d3f95e527b1edbb0f4a1f35ded8e28772158e04
Sam Davies <ssam.daviess@gmail.com>
2017-03-27 01:44:34 -0400

    place server services

--- project/app.js
+++ project/app.js
@@ -1,7 +1,6 @@
 module.exports = function(app) {
   var model = require('./model/models.server.js')();
   require('./services/user.service.server')(app, model);
-  // require('./services/website.service.server.js')(app, model);
+  require('./services/place.service.server')(app, model);
   // require("./services/page.service.server.js")(app, model);
-  // require("./services/widget.service.server.js")(app, model);
 };
