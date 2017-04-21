e81f8398a39b6d959e391ffa99d87ba785447c24
Sam Davies <ssam.daviess@gmail.com>
2017-04-20 22:57:31 -0400

    reminder todo

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ if (!process.env.GOOGLE_CLIENT_ID_ITINERARY_PLANNER) {
   require('dotenv').config();
 }
 
+//TODO UNCOMMIT .ENV FILE AFTER GRADING
+
 app.use(cookieParser());
 app.use(session({
   secret: 'this is the secret', // process.env.SESSION_SECRET
